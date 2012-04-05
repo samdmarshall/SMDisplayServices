5514de14cfb3d461b129a036004fb2a0e8e23674
Sam <sam@SamMBP.local>
2012-04-04 22:11:09 -0400

    making the control visible

--- Classes/SMDSController.m
+++ Classes/SMDSController.m
@@ -35,7 +35,7 @@ THIS SOFTWARE IS PROVIDED BY Sam Marshall ''AS IS'' AND ANY EXPRESS OR IMPLIED W
 - (id)init {
 	self = [super init];
 	if (self) {
-		self.displayview = [[SMDSScreenControl alloc] initWithFrame:CGRectMake(0,0,0,0)];
+		self.displayview = [[SMDSScreenControl alloc] initWithFrame:CGRectMake(0,0,500,200)];
 		self.displays = [self update];
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateDetected:) name:NSApplicationDidChangeScreenParametersNotification object:nil];
 		[self renderDisplays];
